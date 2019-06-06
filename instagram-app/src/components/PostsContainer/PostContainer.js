b46cbcc86d1ee05a8b2637510351c7ee3d1e6ec8
Marcus Kwan <kwan.marcus@gmail.com>
2019-06-06 15:49:29 +1000

    added rendering in postcontainer to display other the other data images and information

--- instagram-app/src/components/PostsContainer/PostContainer.js
+++ instagram-app/src/components/PostsContainer/PostContainer.js
@@ -7,10 +7,26 @@ class PostContainer extends React.Component {
   render() {
     return (
       <>
-        <div>{this.props.postObject.username}</div>
-        <CommentSection
-          postCommentsArray={this.props.postObject.comments}
-        />
+        <div className="poster-thumnail-username">
+          <div className="poster-thumbnail">
+            <img
+              src={this.props.postObject.thumbnailUrl}
+              alt="poster-thumbnail"
+            />
+          </div>
+          <div className="poster-username">
+            {this.props.postObject.username}
+          </div>
+        </div>
+        <div className="poster-image">
+          <img src={this.props.postObject.imageUrl} alt="poster-image" />
+        </div>
+        <div className="icons">
+          <div className="heart-icon">Insert heart icon here</div>
+          <div className="comment-icon">Insert comment icon here</div>
+        </div>
+        <div className="likes">{this.props.postObject.likes} likes</div>
+        <CommentSection postCommentsArray={this.props.postObject.comments} />
       </>
     );
   }
